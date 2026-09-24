40aac79908e2f8c98622e1280770c53da4184404
Hanok John <hanokjohn2004@gmail.com>
2026-09-24 11:03:09 +0530

    ch02: Tokenized full story using regex - 4690 total tokens! (^_^)

---
+++ ch02_text_data/tokenizer.py
@@ -0,0 +1,16 @@
+with open("../data/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+print("Total number of characters :", len(raw_text))
+print(raw_text[:99])
+
+import re
+text = "Hello, world. Is this-- a test?"
+result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+result = [item.strip() for item in result if item.strip()]
+print(result)
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print("The length of total tokens :", len(preprocessed))
+print(preprocessed[:30])
