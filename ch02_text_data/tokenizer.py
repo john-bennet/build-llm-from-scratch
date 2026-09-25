cab5a8aa141f1c4689d486d01c3ea3fc8326b71c
Hanok John <hanokjohn2004@gmail.com>
2026-09-25 13:40:49 +0530

    ch02 : I learned how encoding and decoding of tokens happens using SimpleTokenizerV1. And I learnt <|unk|> and <|endoftext|>

--- ch02_text_data/tokenizer.py
+++ ch02_text_data/tokenizer.py
@@ -5,7 +5,7 @@ print("Total number of characters :", len(raw_text))
 print(raw_text[:99])
 
 import re
-text = "Hello, world. Is this-- a test?"
+text = "It's the last he painted, you know, Mrs. Gisburn said with pride"
 result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
 result = [item.strip() for item in result if item.strip()]
 print(result)
@@ -14,3 +14,32 @@ preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 print("The length of total tokens :", len(preprocessed))
 print(preprocessed[:30])
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer, token in enumerate(all_tokens)}
+print(len(vocab.items()))
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+tokenizer = SimpleTokenizerV1(vocab)
+text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable
+pride."""
+ids = tokenizer.encode(text)
+print(ids)
+print(tokenizer.decode(ids))
